d2580b669f05d55c579757d5a389902237d94702
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-05-14 13:57:23 +0100

    Built the file that pass pycodestyle

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Test file Storage"""
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test File Storage"""
+
+    def test_pep8_file_storage(self):
+        """tests pep8 style"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
