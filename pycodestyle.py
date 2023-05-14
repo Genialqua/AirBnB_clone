34155aa7a1ae336e8c797d57e91300d68840f2ce
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-05-14 10:10:49 +0100

    A code that passes the pycodestyle

--- pycodestyle.py
+++ pycodestyle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""This code tests the pycodestyle"""
 
-# This code tests the pycodestyle
+
 class PycodestyleTest:
-    # The function to do the test
+
+    """The function to do the test"""
+
     def pycodestyle_test(self):
-        # The code is stated as follows
+        """The code is stated as follows"""
         print("Hello, this code is compliant")
