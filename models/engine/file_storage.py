2522dd4c84769eeecf90faf37a737825dbc36533
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-05-14 14:35:15 +0100

    Updated the storage file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This file contains the file storage class
+"""
+from json import dump, load, dumps
+from os.path import exists
+from models import base_model
+from models import user
+from models import state
+from models import city
+from models import amenity
+from models import place
+from models import review
+
