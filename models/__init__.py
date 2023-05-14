283dfa3566b96d3da598b5cb9c1852c22e30535f
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-05-14 14:25:13 +0100

    Updated the model file

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+FileStorage Instance
+"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+It contains the Amenity class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    The Amenity class
+    """
+    name = ""
